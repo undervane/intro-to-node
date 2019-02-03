014ed00988591ebdb2d75c731f7567182249a3c8
Undervane <sergio@mipigu.com>
2019-02-03 19:22:03 +0100

    [Chapter 1] Added post request with body

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express from 'express'
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json())
 
 var person = { name: 'Sergio' }
 
 app.get('/hello', (req, res) => {
 
-    res.send(`Hello from server, ${person.name}!`)
+    const name = 'Sergio'
+
+    res.send(`Hello from server, ${name}!`)
+})
+
+app.post('/name', (req, res) => {
+
+    let name = req.body.name
+
+    if (!name){
+        res.send('Name is required')
+        return;
+    }
+
+    person.name = name
+
+    res.send(`Name changed to ${name}`)
 
 })
 
